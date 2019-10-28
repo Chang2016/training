8938e2ba6ed54a513b1221a119375978c8c46c94
Michael Strupp <m.strupp@gmx.de>
2019-10-28 22:38:36 +0100

    var type

--- java-new-version-test/src/main/java/training/java10/Java10.java
+++ java-new-version-test/src/main/java/training/java10/Java10.java
@@ -3,7 +3,15 @@ package training.java10;
 public class Java10 {
   private static String name = "";
 
+  void varType() {
+    var firstName = "Anton";
+    var age = 99;
+    System.out.println(firstName.getClass());
+    System.out.println(((Object)age).getClass());
+  }
+
   public static void main(String [] args) {
-    System.out.println("Hallo " + name);
+    Java10 java10 = new Java10();
+    java10.varType();
   }
 }
