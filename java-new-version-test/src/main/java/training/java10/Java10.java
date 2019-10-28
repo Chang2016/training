c17516e223e17bca267751350b77857468135d5e
Michael Strupp <m.strupp@gmx.de>
2019-10-28 22:50:52 +0100

    var map

--- java-new-version-test/src/main/java/training/java10/Java10.java
+++ java-new-version-test/src/main/java/training/java10/Java10.java
@@ -1,9 +1,17 @@
 package training.java10;
 
+import java.util.Map;
+
 public class Java10 {
   private static String name = "";
 
+  static Map<Integer, String> varMap() {
+    var map = Map.of(1, "lala", 2, "huhu");
+    return map;
+  }
+
   public static void main(String [] args) {
-    System.out.println("Hallo " + name);
+    Map<Integer, String> map = varMap();
+    map.entrySet().forEach(System.out::println);
   }
 }
