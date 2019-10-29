ae59ee0f006fccdf2cf4c27c0f8cdf21e79795b9
Michael Strupp <m.strupp@gmx.de>
2019-10-29 22:39:49 +0100

    var Predicate

--- java-new-version-test/src/main/java/training/java10/Java10.java
+++ java-new-version-test/src/main/java/training/java10/Java10.java
@@ -1,9 +1,18 @@
 package training.java10;
 
 import java.util.Map;
+import java.util.function.Predicate;
 
 public class Java10 {
   private static String name = "";
+
+  void lambdaVar() {
+    //var isEven = n -> n % 2 == 0;
+    //var isEmpty = String::isEmpty;
+    Predicate<Long> isEven = n -> n % 2 == 0;
+    var isOdd = isEven.negate();
+    System.out.println(isEven.test(5L));
+  }
 
   void varType() {
     var firstName = "Anton";
@@ -23,5 +32,6 @@ public class Java10 {
     map.entrySet().forEach(System.out::println);
     Java10 java10 = new Java10();
     java10.varType();
+    java10.lambdaVar();
   }
 }
