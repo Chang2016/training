057041c8a78d6ec5f58b3235eb005f2e7526142f
Michael Strupp <m.strupp@gmx.de>
2019-10-29 23:21:25 +0100

    unmodifiableCollections

--- java-new-version-test/src/main/java/training/java10/Java10.java
+++ java-new-version-test/src/main/java/training/java10/Java10.java
@@ -1,10 +1,28 @@
 package training.java10;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 
 public class Java10 {
   private static String name = "";
+
+  void unmodifiableCollections() {
+    String[] words = {"Hello", "World"};
+    List<String> asList = Arrays.asList(words);
+    List<String> of = List.of(words);
+    System.out.println(asList.getClass());
+    System.out.println(of.getClass());
+    asList.stream().forEach(System.out::println);
+    of.stream().forEach(System.out::println);
+    words[1] = "Earth";
+    //UnsupportedOperationException
+    //of.set(1, "Moon");
+    asList.set(1, "Moon");
+    asList.stream().forEach(System.out::println);
+    of.stream().forEach(System.out::println);
+  }
 
   void lambdaVar() {
     //var isEven = n -> n % 2 == 0;
@@ -28,10 +46,11 @@ public class Java10 {
   }
 
   public static void main(String [] args) {
-    Map<Integer, String> map = varMap();
-    map.entrySet().forEach(System.out::println);
+    //Map<Integer, String> map = varMap();
+    //map.entrySet().forEach(System.out::println);
     Java10 java10 = new Java10();
-    java10.varType();
-    java10.lambdaVar();
+    //java10.varType();
+    //java10.lambdaVar();
+    java10.unmodifiableCollections();
   }
 }
